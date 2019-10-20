a0bebb4655afe2b4e11aeadfe4453c2895a15f76
Abuka <maacsgaming@gmail.com>
2019-10-20 21:01:07 +0300

    Add login button fix

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,37 +1,42 @@
 import React, { Component } from 'react'
 import './Login.css'
+import Aux from '../../HOC/Auxillary'
+import Footer from '../Login-Footer/Footer'
 
 class Login extends Component {
   render () {
     return (
-      <div className='container'>
-        <h1 className='logo'>
+      <Aux>
+        <div className='container'>
+          <h1 className='logo'>
           ESOTA
-        </h1>
-        <div className='content'>
-          <div className='panel' id='login'>
-            <h3>Log in to your account</h3>
-            <form action='/login' method='post'>
-              <div className='form-group'>
-                <label htmlFor='country'>Country</label>
-                <select className='form-control'>
-                  <option selected>Tanzania</option>
-                  <option value='1'>Kenya</option>
-                  <option value='2'>Uganda</option>
-                </select>
+          </h1>
+          <div className='content'>
+            <div className='panel' id='login'>
+              <h3>Log in to your account</h3>
+              <form action='/login' method='post'>
+                <div className='form-group'>
+                  <label htmlFor='country'>Country</label>
+                  <select className='form-control'>
+                    <option defaultValue='true'>Tanzania</option>
+                    <option value='1'>Kenya</option>
+                    <option value='2'>Uganda</option>
+                  </select>
+                </div>
+                <div className='form-group'>
+                  <label htmlFor='phone number'>Phone Number</label>
+                  <input autoComplete='off' className='form-control password' id='phone' name='phone' placeholder='Phone Number' tabIndex='2' type='number' required />
+                </div>
+                <button className='btn btn-primary btn-lg btn-block' name='commit' tabIndex='3' type='submit' value='Log In'>Log In</button>
+              </form>
+              <div className='panel-footer'>
+              Welcome To Our Official Web Application
               </div>
-              <div className='form-group'>
-                <label htmlFor='phone number'>Phone Number</label>
-                <input autoComplete='off' className='form-control password' id='phone' name='phone' placeholder='Phone Number' tabIndex='2' type='number' />
-              </div>
-              <button className='btn btn-primary btn-lg btn-block' name='commit' tabIndex='3' type='submit' value='Log In'>Log In</button>
-            </form>
-            <div className='panel-footer'>
-              Welcome to ESOTA Official Web Application
             </div>
           </div>
         </div>
-      </div>
+        <Footer />
+      </Aux>
     )
   }
 }
