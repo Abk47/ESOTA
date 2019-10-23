780b38e221d82a142365ef7605bb80aad93704ea
Abuka <maacsgaming@gmail.com>
2019-10-23 17:00:48 +0300

    Add pagination component

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import ProductList from '../ProductList/ProductList'
 import Header from '../Header/Header'
+import ProductList from '../ProductList/ProductList'
+import Pagination from '../Pagination/Pagination'
 
 const Grid = (props) => (
   <main class='col-md-9'>
     <Header />
     <ProductList />
+    <Pagination />
   </main>
 )
 
