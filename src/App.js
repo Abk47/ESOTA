1c009040ee0a8d0150a3775dec4641584e9dff8c
Abuka <maacsgaming@gmail.com>
2019-10-23 15:57:11 +0300

    Few fixes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import './App.css'
 import ProductList from './components/ProductList/ProductList'
-// import Login from './components/Login/Login'
 
 function App () {
   return (
     <div className='App'>
-      {/* <Login /> */}
       <ProductList />
     </div>
   )
