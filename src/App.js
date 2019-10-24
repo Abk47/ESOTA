d5a0624ac9d204e251202d8be2791444916accfc
Abuka <maacsgaming@gmail.com>
2019-10-24 16:12:08 +0300

    Add banner component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import Wrapper from './components/Wrapper/Wrapper'
+import Banner from './components/Banner/Banner'
 
 function App () {
   return (
     <div className='App'>
-      <header className='heading mb-3 mt-3' style={{ textAlign: 'center', fontWeight: 'bold', fontSize: '30px' }}>
+      <Banner />
+      <header className='heading mb-2 mt-3' style={{ textAlign: 'center', fontWeight: 'bold', fontSize: '30px' }}>
         PRODUCTS
       </header>
       <Wrapper />
