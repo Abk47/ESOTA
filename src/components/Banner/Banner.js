295fd3c576f152c1531f2f6cb63e892a699afbd3
Abuka <maacsgaming@gmail.com>
2019-10-25 10:43:58 +0300

    Add Banner component

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Banner.css'
+
+const Banner = (props) => (
+  <section className='section-intro padding-y-sm mb-4'>
+    <div className='container'>
+
+      <div className='intro-banner-wrap'>
+        <img src='images/banner.jpg' alt='banner' className='img-fluid rounded' />
+      </div>
+
+    </div>
+  </section>
+)
+
+export default Banner
