19a4e02cf0f43bae698c20e21d8f59c6c49fd414
Abuka <maacsgaming@gmail.com>
2019-11-01 00:52:03 +0300

    Remove HOC

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,20 +3,17 @@ import Wrapper from '../Wrapper/Wrapper'
 import Banner from '../Banner/Banner'
 import Layout from '../Layout/Layout'
 import Advert from '../Advert/Advert'
-import Aux from '../../HOC/Auxillary'
 
 const Home = (props) => {
   return (
-    <Aux>
-      <Layout>
-        <Banner />
-        <header className='heading mb-2 mt-3' style={{ textAlign: 'center', fontWeight: 'bold', fontSize: '30px' }}>
+    <Layout>
+      <Banner />
+      <header className='heading mb-2 mt-3' style={{ textAlign: 'center', fontWeight: 'bold', fontSize: '30px' }}>
           PRODUCTS
-        </header>
-        <Wrapper />
-        <Advert />
-      </Layout>
-    </Aux>
+      </header>
+      <Wrapper />
+      <Advert />
+    </Layout>
   )
 }
 
