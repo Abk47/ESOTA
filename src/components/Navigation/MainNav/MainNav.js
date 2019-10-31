7207ffc0b8ef6cbce6dbc1ed2917026ccfab58b4
Abuka <maacsgaming@gmail.com>
2019-11-01 01:04:02 +0300

    Add Link tags

--- src/components/Navigation/MainNav/MainNav.js
+++ src/components/Navigation/MainNav/MainNav.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './MainNav.css'
+import { Link } from 'react-router-dom'
 
 const MainNav = (props) => (
   <nav className='navbar navbar-main navbar-expand-md navbar-light border-top border-bottom' style={{ boxShadow: '0 2px 4px -1px rgba(0,0,0,0.25)' }}>
@@ -7,15 +8,15 @@ const MainNav = (props) => (
       <div className='navbar-collapse collapse show' id='main_nav'>
         <ul className='navbar-nav' style={{ fontWeight: 'bold' }}>
           <li className='nav-item'>
-            <a className='nav-link pl-0'><i className='fa fa-user' style={{ color: '#ff2500' }} /> My Profile </a>
+            <Link to='/profile' className='nav-link pl-0'><i className='fa fa-user' style={{ color: '#ff2500' }} /> My Profile </Link>
           </li>
           <li className='nav-item'>
-            <a className='nav-link'><i className='fa fa-credit-card' style={{ color: '#ff2500' }} /> Payment</a>
+            <Link to='/payment' className='nav-link'><i className='fa fa-credit-card' style={{ color: '#ff2500' }} /> Payment</Link>
           </li>
         </ul>
         <ul className='navbar-nav ml-auto' style={{ fontWeight: 'bold' }}>
           <li className='nav-item'>
-            <a className='nav-link'><i className='fa fa-sign-out' style={{ color: '#ff2500' }} /> Sign Out</a>
+            <Link to='/logout' className='nav-link'><i className='fa fa-sign-out' style={{ color: '#ff2500' }} /> Sign Out</Link>
           </li>
         </ul>
       </div>
