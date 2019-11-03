7e94aaaf26f0b3e1db07a3f54522824239900e82
Abuka <maacsgaming@gmail.com>
2019-11-03 16:23:51 +0300

    Add Link tags

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Footer.css'
 
 const Footer = (props) => (
@@ -9,39 +10,39 @@ const Footer = (props) => (
         <div className='col-xs-6 col-md-3'>
           <h6>Categories</h6>
           <ul className='footer-links'>
-            <li><a href='#'>Watches</a></li>
-            <li><a href='#'>Cinema</a></li>
-            <li><a href='#'>Fashion</a></li>
-            <li><a href='#'>Cologne</a></li>
-            <li><a href='#'>Furniture</a></li>
+            <li><Link to='/'>Watches</Link></li>
+            <li><Link to='/'>Cinema</Link></li>
+            <li><Link to='/'>Fashion</Link></li>
+            <li><Link to='/'>Cologne</Link></li>
+            <li><Link to='/'>Furniture</Link></li>
           </ul>
         </div>
 
         <div className='col-xs-6 col-md-3'>
           <h6>Company</h6>
           <ul className='footer-links'>
-            <li><a href='#'>About Us</a></li>
-            <li><a href='#'>Contact Us</a></li>
-            <li><a href='#'>Careers</a></li>
+            <li><Link to='/'>About Us</Link></li>
+            <li><Link to='/'>Contact Us</Link></li>
+            <li><Link to='/'>Careers</Link></li>
           </ul>
         </div>
 
         <div className='col-xs-6 col-md-3'>
           <h6>Help</h6>
           <ul className='footer-links'>
-            <li><a href='#'>How it works</a></li>
-            <li><a href='#'>Become a vendor</a></li>
-            <li><a href='#'>Open dispute</a></li>
+            <li><Link to='/'>How it works</Link></li>
+            <li><Link to='/'>Become a vendor</Link></li>
+            <li><Link to='/'>Open dispute</Link></li>
           </ul>
         </div>
 
         <div className='col-xs-6 col-md-3'>
           <h6>Quick Links</h6>
           <ul className='footer-links'>
-            <li><a href='#'>My profile</a></li>
-            <li><a href='#'>Delivery</a></li>
-            <li><a href='#'>Pending products</a></li>
-            <li><a href='#'>Payment</a></li>
+            <li><Link to='/profile'>My profile</Link></li>
+            <li><Link to='/'>Delivery</Link></li>
+            <li><Link to='/'>Pending products</Link></li>
+            <li><Link to='/'>Payment</Link></li>
           </ul>
         </div>
 
@@ -52,15 +53,15 @@ const Footer = (props) => (
       <div className='row'>
         <div className='col-md-8 col-sm-6 col-xs-12'>
           <p className='copyright-text'>Copyright &copy; {new Date().getFullYear()} All Rights Reserved by
-            <a href='#'> ESOTA</a>.
+            <Link to='/'> ESOTA</Link>.
           </p>
         </div>
 
         <div className='col-md-4 col-sm-6 col-xs-12'>
           <ul className='social-icons'>
-            <li><a className='facebook' href='#'><i className='fa fa-facebook' /></a></li>
-            <li><a className='twitter' href='#'><i className='fa fa-twitter' /></a></li>
-            <li><a className='linkedin' href='#'><i className='fa fa-linkedin' /></a></li>
+            <li><Link className='facebook' href='/'><i className='fa fa-facebook' /></Link></li>
+            <li><Link className='twitter' href='/'><i className='fa fa-twitter' /></Link></li>
+            <li><Link className='linkedin' href='/'><i className='fa fa-linkedin' /></Link></li>
           </ul>
         </div>
       </div>
