fb72a2d4f1f8b1903db7faf45d540222d363c566
Abuka <maacsgaming@gmail.com>
2019-11-06 21:17:38 +0300

    Add route path

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Login from './components/Login/Login'
 import Home from './components/Home/Home'
+import Profile from './components/Profile/Profile'
+import Pending from './components/Pending/Pending'
+import Delivery from './components/Delivery/Delivery'
+import Payment from './components/Payment/Payment'
 
 function App () {
   return (
@@ -10,6 +14,10 @@ function App () {
       <Switch>
         <Route path='/' exact component={Home} />
         <Route path='/login' component={Login} />
+        <Route path='/profile' component={Profile} />
+        <Route path='/pending' component={Pending} />
+        <Route path='/delivery' component={Delivery} />
+        <Route path='/payment' component={Payment} />
       </Switch>
     </BrowserRouter>
   )
