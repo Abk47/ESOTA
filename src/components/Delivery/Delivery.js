641519eb04533bee58db3785e206e577473b4e7c
Abuka <maacsgaming@gmail.com>
2019-11-06 21:12:58 +0300

    Add Delivery component

---
+++ src/components/Delivery/Delivery.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from '../Layout/Layout'
+import './Delivery.css'
+
+const Pending = (props) => (
+  <Layout>
+    <div className='content'>
+      <div className='title'>
+        <h3>DELIVERED PRODUCTS</h3>
+      </div>
+      
+    </div>
+  </Layout>
+)
+
+export default Pending
